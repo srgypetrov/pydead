ce6563a80f2d4fbd7dff2bd6f4948d3d0e206763
Sergey Petrov <desoulate777@gmail.com>
2017-09-28 12:44:17 +0300

    fix test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,22 @@
-import os
+import pytest
 
 from src.utils import get_dot_relpath
 
 
-def test_get_dot_relpath_directory():
-    relpath = get_dot_relpath(os.path.join(os.getcwd(), 'tests/test_utils/'))
-    assert relpath == 'tests.test_utils'
-
-
-def test_get_dot_relpath_file():
-    relpath = get_dot_relpath(os.path.join(os.getcwd(), 'tests/test_utils/test.py'))
-    assert relpath == 'tests.test_utils.test'
+@pytest.mark.parametrize("base, path, expected", [
+    ('/apps/', '/apps/', '.'),
+    ('/apps', '/apps/', '.'),
+    ('/apps/', '/app/test.py', 'error'),
+    ('/apps/', 'test.py', 'error'),
+    ('/apps/', '/apps/test.py', 'test'),
+    ('/apps/', '/apps/core/test.py', 'core.test'),
+    ('/apps/', '/apps/core/module/', 'core.module'),
+    ('/apps/core/module/', '/apps/', 'error')
+])
+def test_get_dot_relpath(base, path, expected):
+    if expected == 'error':
+        with pytest.raises(AssertionError):
+            dot_path = get_dot_relpath(base, path)
+    else:
+        dot_path = get_dot_relpath(base, path)
+        assert dot_path == expected
